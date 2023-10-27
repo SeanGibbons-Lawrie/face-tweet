41ee466ed6025ad267be5b40add921855e4c6ec4
lj-linessmith <142572937+lj-linessmith@users.noreply.github.com>
2023-10-27 14:21:22 +1300

    added member imgs

--- server/db/seeds/2-members.js
+++ server/db/seeds/2-members.js
@@ -1,9 +1,9 @@
 export async function seed(knex) {
   await knex('members').insert([
-    { id: 1, name: 'Ambitious Aardvark' },
-    { id: 2, name: 'Bamboozled Baboon' },
-    { id: 3, name: 'Curious Capybara' },
-    { id: 4, name: 'Dilapidated Duck' },
-    { id: 5, name: 'Exuberant Elephant' },
+    { id: 1, name: 'Michael' },
+    { id: 2, name: 'Gaby' },
+    { id: 3, name: 'Lucy' },
+    { id: 4, name: 'Elise' },
+    { id: 5, name: 'Gerard' },
   ])
 }
